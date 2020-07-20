98c3e989d5f7cbf83089fd37a8acce4df8aef7c0
vagrant <vagrant@homestead>
2020-07-20 08:09:10 +0000

    webpack 초기화 원복

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+let scripts = [
+	'jquery',
+	'axios',
+	'jquery-ui',
+	'popper.js',
+];
+
+let autoload = [
+	{'jquery': ['$', 'jQuery', 'window.$', 'window.jQuery', 'jquery']},
+];
+
+mix.js('resources/js/app.js', 'public/js/app.js');
+mix.js(['node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'], 'public/js/bootstrap.bundle.min.js');
+mix.sass('resources/sass/app.scss', 'public/css/app.css');
+mix.extract(scripts, 'public/js/vendor');
+mix.autoload(autoload);
